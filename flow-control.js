44f32535989daf352ca4dd2a0eb9a5e6349c37a3
bold-mixer-0681 <renee.pujol@gmail.com>
2020-05-14 00:04:49 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,8 +25,10 @@ function ageChecker(age) {
     }
   else if (age >= 12){
     return "You are a kid"
+    }
+  else if (age >= 20){
+    return "You are a grownup"
   }
-
 };
 
 function ternaryTeenager(age) {
