3aa005716f1c77a3405aabaa49c19686ff370996
bold-mixer-0681 <renee.pujol@gmail.com>
2020-05-14 00:00:00 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,9 @@
 function basicTeenager(age) {
-  if ( 13 <= age <= 19){
+  if ( 13 <= age && age <= 19){
       return "You are a teenager!"
       }
   else {
-      return 'undefined'
+      return undefined
       }
 
 };
